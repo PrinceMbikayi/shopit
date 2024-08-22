a975f6c28190203e445f2e5472c73f6888c3745e
Harun Biswas Rubel <harunbiswasrubel@gmail.com>
2024-08-23 00:44:48 +0600

    update

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,14 +1,14 @@
 // Checks if user is authenticated or not 
-import catchAsyncErrors from './catchAsyncErrors.js'
-import ErrorHandler from '../utils/errorHandler.js'
-import User from '../models/user.js'
 import jwt from 'jsonwebtoken';
+import User from '../models/user.js';
+import ErrorHandler from '../utils/errorHandler.js';
+import catchAsyncErrors from './catchAsyncErrors.js';
 
 export const isAuthenticatedUser = catchAsyncErrors(async (req, res, next) => {
     //const { token } = req.cookies;
     const authHeader = req.headers.authorization;
     const token = authHeader && authHeader.split(' ')[1];
-
+ 
     if(!token){
         return next(new ErrorHandler('Login first to access this ressource', 401));
     }
